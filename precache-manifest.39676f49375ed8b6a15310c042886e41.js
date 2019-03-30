9df27c360090825cc6e80591cdf28a6ac0d54b0d
Christopher Fuentes <fuenteschris75@gmail.com>
2019-03-30 11:34:34 -0600

    Updates

---
+++ precache-manifest.39676f49375ed8b6a15310c042886e41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "844bbe397b8fb490ae61",
+    "url": "/taskboard/static/js/runtime~main.984add8f.js"
+  },
+  {
+    "revision": "3f07914adaeb3109cc3c",
+    "url": "/taskboard/static/js/main.71743095.chunk.js"
+  },
+  {
+    "revision": "ae05e698d873c9f739e5",
+    "url": "/taskboard/static/js/2.853cd350.chunk.js"
+  },
+  {
+    "revision": "3f07914adaeb3109cc3c",
+    "url": "/taskboard/static/css/main.6a4d5c48.chunk.css"
+  },
+  {
+    "revision": "da8a0290d820cded6b0697b89e8ac30e",
+    "url": "/taskboard/index.html"
+  }
+];
