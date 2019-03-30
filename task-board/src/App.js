91365d3e397555970d560b4ed6601975c67c53ea
Christopher Fuentes <fuenteschris75@gmail.com>
2019-03-29 18:38:09 -0600

    Modificando index de ejemplo

--- task-board/src/App.js
+++ task-board/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edita <code>src/App.js</code> y guárdalo para recargar.
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Aprende React
           </a>
         </header>
       </div>
