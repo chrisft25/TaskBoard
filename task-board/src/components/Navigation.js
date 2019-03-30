b94186059be1a3249003beb643378d97f7841ebb
Christopher Fuentes <fuenteschris75@gmail.com>
2019-03-30 10:26:17 -0600

    Modificación en href

--- task-board/src/components/Navigation.js
+++ task-board/src/components/Navigation.js
@@ -4,7 +4,7 @@ class Navigation extends Component{
 render(){
     return(
         <nav className="navbar navbar-dark bg-dark">
-        <a className="text-white" href="#">{ this.props.title } <span className="badge badge-pill badge-light ml-2">{this.props.ntasks}</span></a>
+        <a href="/#" className="text-white">{ this.props.title } <span className="badge badge-pill badge-light ml-2">{this.props.ntasks}</span></a>
         
         </nav>
     )
